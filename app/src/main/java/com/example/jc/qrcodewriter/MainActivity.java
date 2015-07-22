89bd817aba3ae957330c1dd559711478c50934cc
Joshua Calloway <joshua.calloway@gmail.com>
2015-07-22 15:37:48 -0500

    first

---
+++ app/src/main/java/com/example/jc/qrcodewriter/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.jc.qrcodewriter;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+;import net.glxn.qrgen.android.QRCode;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final EditText myText = (EditText)findViewById(R.id.myEditText);
+
+        Button myButton = (Button)findViewById(R.id.myButton);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                generateQRCode(myText.getText());
+            }
+        });
+
+
+
+    }
+
+    ImageView myImageView;
+
+    private void generateQRCode(Editable newText) {
+        myImageView = (ImageView) findViewById(R.id.myImageView);
+        String text = newText.toString();
+        Bitmap myBitmap = QRCode.from(text).bitmap();
+        //myBitmap.reconfigure(300, 300, Bitmap.Config.ALPHA_8);
+//        myBitmap.setHeight(200);
+
+        ImageView myImage = (ImageView) findViewById(R.id.myImageView);
+        myImage.setImageBitmap(myBitmap);
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
